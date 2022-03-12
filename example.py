215d0505691660123b5bbeebd4c3f9a9367237fc
Eric Seifert <seiferteric@gmail.com>
2022-03-12 13:17:37 -0800

    wip

--- example.py
+++ example.py
@@ -1,14 +1,35 @@
+from enum import Enum
+from weblit import Weblit, Select
 
-from weblit import Weblit
+class Planet(Enum):
+	MERCURY = 3.7
+	VENUS   = 8.87
+	EARTH   = 9.807 
+	MARS    = 3.721
+	JUPITER = 24.79
+	SATURN  = 10.44
+	URANUS  = 8.69
 
-MARS_G=3.721
-EARTH_G=9.807
 
-app = Weblit("Weight on Mars")
+wapp = Weblit("Weight On The Planets")
 
-@app.Form
-def weight_on_mars(weight_on_earth: float=0):
-	return (MARS_G/EARTH_G)*float(weight_on_earth)
+@wapp.Form({'planet': Select()})
+def weight_on_planet(planet: Planet=Planet.MERCURY, weight_on_earth: float=0) -> float:
+	if isinstance(planet, Planet):
+		return (planet.value/Planet.EARTH.value)*float(weight_on_earth)
+	
+	return (Planet[planet].value/Planet.EARTH.value)*float(weight_on_earth)
+
+
+
+# @wapp.Page("/")
+# def index(self):
+# 	self.text("Hello World!")
+# 	self.image("mars.jpg")
+# 	self.form("weight_on_mars")
+
 
 if __name__ == '__main__':
-	app.run()
+	
+
+	wapp.run()
--- weblit.py
+++ weblit.py
@@ -1,5 +1,6 @@
 import functools
 import inspect
+from enum import Enum
 from flask import Flask, request, render_template
 
 from pprint import pformat
@@ -9,34 +10,51 @@ class Weblit(object):
 	def __init__(self, name):
 		self.name = name or __name__
 		self.app = Flask(self.name)
+		self.forms = []
 
 		@self.app.route("/")
-		def hello_world():
-		    return "<p>{}</p>".format(self.name)
+		def index():
+		    return render_template('index.html', app=self)
 
-	def Form(self, func):
+	def Form(self, in_format = {}, out_format={}):
+
+		def decorator_form(func):
+			self.forms.append(func.__name__)
+			def view_wrapper(*args, **kwargs):
+				return render_template('form.html', fname=func.__name__, sig=inspect.signature(func), helpers=TemplateHelpers, param_format=in_format)
+
+			def form_wrapper(*args, **kwargs):
+				return render_template('form.html', fname=func.__name__, sig=inspect.signature(func), helpers=TemplateHelpers, result=func(**request.form))
+
+			self.app.add_url_rule("/{}".format(func.__name__), view_func=view_wrapper)
+			self.app.add_url_rule("/{}".format(func.__name__), methods=["POST"], view_func=form_wrapper)
+			return func
+		return decorator_form
 
 
-		def view_wrapper(*args, **kwargs):
-			return render_template('form.html', fname=func.__name__, sig=inspect.signature(func), helpers=TemplateHelpers)
-
-		def form_wrapper(*args, **kwargs):
-			return render_template('form_output.html', fname=func.__name__, sig=inspect.signature(func), helpers=TemplateHelpers, output=func(**request.form))
-
-		self.app.add_url_rule("/{}".format(func.__name__), view_func=view_wrapper)
-		self.app.add_url_rule("/{}".format(func.__name__), methods=["POST"], view_func=form_wrapper)
-		return func
+            
 
 	def run(self):
 		self.app.run()
 
 
-
+class Select(object):
+	multi: False
+	def __init__(self, multi=False):
+		self.multi = multi
 
 
 class TemplateHelpers(object):
-	def type(annot):
-		if annot is float:
+	def type(param):
+		if param.annotation is float:
 			return "number"
+		if issubclass(param.annotation, Enum):
+			return "radio"
 		else:
 			return "text"
+	def default(param):
+		if param.default is inspect._empty:
+			return ''
+		else:
+			return param.default
+	
